fd03d42a0c6cfaf169e134538e22544206ff9cec
Rustem <rustemazimov1999@gmail.com>
2017-12-29 19:05:25 +0400

    Fixed bug about AlertDialog usage

--- app/src/main/java/com/apps/romeo/adlarinmenasi/MainActivity.java
+++ app/src/main/java/com/apps/romeo/adlarinmenasi/MainActivity.java
@@ -1,22 +1,16 @@
 package com.apps.romeo.adlarinmenasi;
 
-import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.StrictMode;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -64,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
                 String name = ((EditText) findViewById(R.id.name_edit_text)).getText().toString().trim();
                 if (name.isEmpty())
                 {
+                    progressDialog.hide();
                     meaningTextView.setText("Ad daxil edilməyib");
                     return;
                 }
